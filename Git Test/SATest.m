92950f77855c24250952862b3dab67cd4ceb2192
Gino Heetkamp <ginoheetkamp@iMac-van-Gino.local>
2014-06-01 09:40:50 +0200

    Added a test class

---
+++ Git Test/SATest.m
@@ -0,0 +1,13 @@
+//
+//  SATest.m
+//  Git Test
+//
+//  Created by Gino Heetkamp on 01-06-14.
+//  Copyright (c) 2014 Sanino. All rights reserved.
+//
+
+#import "SATest.h"
+
+@implementation SATest
+
+@end
